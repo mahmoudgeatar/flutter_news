b2fadf7f3423bc2bf9e306fbb553e5c286e0b720
mahmoudgeater <zidane_hoda2200@yahoo.com>
2020-05-01 11:01:03 +0200

    flutter

---
+++ lib/screens/tabs_screen/singlePost.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class singlePost extends StatefulWidget {
+  @override
+  _singlePostState createState() => _singlePostState();
+}
+
+class _singlePostState extends State<singlePost> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(child: Text('Data')),
+    );
+  }
+}
